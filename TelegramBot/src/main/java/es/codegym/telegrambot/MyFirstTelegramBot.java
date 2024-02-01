1ec489f8104e30b237f57cdf73089d84e7617d86
Ing Guillermo Malagon <guillomal373@gmail.com>
2024-02-01 11:52:23 -0500

    Adding images

--- TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -32,8 +32,9 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
         // TODO: escribiremos la funcionalidad principal del bot aquí
         if ( getMessageText().equals("/start") ){
-            System.out.println("yeah paso 1");
             setUserGlory(0);//Primero vamos a dar puntos por ingresar
+            System.out.println("yeah paso 1");
+            sendPhotoMessageAsync("step_1_pic");
             sendTextMessageAsync(STEP_1_TEXT,
                     Map.of("Hackear la nevera, +20 de fama","step_1_btn"));//Vamos a traer variables de otra clase - CREANDO UN BOTÓN: Título del botón e identificador del botón
         }
@@ -42,6 +43,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         if( getCallbackQueryButtonKey().equals("step_1_btn") ){
             setUserGlory(getUserGlory()+20);
             System.out.println("yeah paso 2 -> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("step_2_pic");
             sendTextMessageAsync(STEP_2_TEXT,
                     Map.of("¡Tomar una salchicha! +20 de fama", "step_2_btn",
                             "¡Tomar un pescado! +20 de fama", "step_2_btn",
@@ -52,6 +54,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         if( getCallbackQueryButtonKey().equals("step_2_btn") ){
             setUserGlory(getUserGlory()+20);
             System.out.println("yeah paso 3-> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("step_3_pic");
             sendTextMessageAsync(STEP_3_TEXT,
                     Map.of("Hackear el robot aspiradora +30 de fama", "step_3_btn"));
         }
@@ -60,6 +63,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         if( getCallbackQueryButtonKey().equals("step_3_btn") ){
             setUserGlory(getUserGlory()+30);
             System.out.println("yeah paso 4-> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("step_4_pic");
             sendTextMessageAsync(STEP_4_TEXT,
                     Map.of("Enviar al robot aspitador por comida +30 de fama", "step_4_btn",
                             "Dar un paseo encima del robot +30 de fama", "step_4_btn",
@@ -70,6 +74,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         if( getCallbackQueryButtonKey().equals("step_4_btn") ){
             setUserGlory(getUserGlory()+30);
             System.out.println("yeah paso 5-> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("step_5_pic");
             sendTextMessageAsync(STEP_5_TEXT,
                     Map.of("Ponerse la GoPro y encenderla +40 de fama", "step_5_btn"));
         }
@@ -78,15 +83,18 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         if( getCallbackQueryButtonKey().equals("step_5_btn") ){
             setUserGlory(getUserGlory()+40);
             System.out.println("yeah paso 6-> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("step_6_pic");
             sendTextMessageAsync(STEP_6_TEXT,
-                    Map.of("Pescado +30 de fama", "step_6_btn",
-                            "Aceituna +30 de fama", "step_6_btn"));
+                    Map.of("Correr por los tejados +40 de fama", "step_6_btn",
+                            "Atacar a otros gatos +40 de fama", "step_6_btn",
+                            "Atacar a unos perrors +40 de fama", "step_6_btn"));
         }
 
         //Que pasa si se da click en otro botón...
         if( getCallbackQueryButtonKey().equals("step_6_btn") ){
             setUserGlory(getUserGlory()+30);
             System.out.println("yeah paso 7-> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("step_7_pic");
             sendTextMessageAsync(STEP_7_TEXT,
                     Map.of("Hackear la contraseña de la computadora +50 de fama", "step_7_btn"));
         }
@@ -95,6 +103,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         if( getCallbackQueryButtonKey().equals("step_7_btn") ){
             setUserGlory(getUserGlory()+50);
             System.out.println("yeah paso 8-> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("step_8_pic");
             sendTextMessageAsync(STEP_8_TEXT,
                     Map.of("Convertirse en gato hacker finalmente", "step_8_btn"));
         }
@@ -103,6 +112,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         if( getCallbackQueryButtonKey().equals("step_8_btn") ){
             setUserGlory(getUserGlory());
             System.out.println("yeah paso 9-> "+getUserGlory()+" puntos");
+            sendPhotoMessageAsync("final_pic");
             sendTextMessageAsync(FINAL_TEXT, Map.of());
         }
 
