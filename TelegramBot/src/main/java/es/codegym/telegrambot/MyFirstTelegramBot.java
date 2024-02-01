cdf73bff130b83406170fa0243f8696b3613a7f8
Ing Guillermo Malagon <guillomal373@gmail.com>
2024-02-01 10:59:07 -0500

    Adding some buttons

--- TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -9,6 +9,9 @@ import java.awt.*;
 import java.time.Instant;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import static es.codegym.telegrambot.TelegramBotContent.*;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
@@ -27,24 +30,80 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     /*Esta es la respuesta a cada interacción a nuestro BOT*/
     public void onUpdateEventReceived(Update update) {
 
-        //https://howtodoinjava.com/java/date-time/java8-datetimeformatter-example/
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a");
-        String miNombre = getBotUsername();
-        //formateando el string _italic_     *negrita*
-        String message = "Hola _"+getCurrentClientUserName()+"_ son las "+LocalTime.now().format(dtf)+", mi nombre es *"+miNombre+"*, gracias por escribir --> "+getMessageText();
-        System.out.println(message);
-
         // TODO: escribiremos la funcionalidad principal del bot aquí
         if ( getMessageText().equals("/start") ){
-            sendTextMessageAsync("BIENVENIDO FUTURO PROGRAMADOR "+message);
+            System.out.println("yeah paso 1");
+            setUserGlory(0);//Primero vamos a dar puntos por ingresar
+            sendTextMessageAsync(STEP_1_TEXT,
+                    Map.of("Hackear la nevera, +20 de fama","step_1_btn"));//Vamos a traer variables de otra clase - CREANDO UN BOTÓN: Título del botón e identificador del botón
         }
 
-        if ( getMessageText().toUpperCase().contains("HOLA") ){
-            sendTextMessageAsync("¿Cuál es tÚ nombre? "+ message);
+        //Que pasa si se da click en un botón...
+        if( getCallbackQueryButtonKey().equals("step_1_btn") ){
+            setUserGlory(getUserGlory()+20);
+            System.out.println("yeah paso 2 -> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(STEP_2_TEXT,
+                    Map.of("¡Tomar una salchicha! +20 de fama", "step_2_btn",
+                            "¡Tomar un pescado! +20 de fama", "step_2_btn",
+                            "¡Tomar una lata de pepinillos! +20 de fama", "step_2_btn"));
         }
 
-        if ( getMessageText().toUpperCase().contains("I NOMBRE ES") ){
-            sendTextMessageAsync("Es un placer yo soy *GATO*");
+        //Que pasa si se da click en otro botón...
+        if( getCallbackQueryButtonKey().equals("step_2_btn") ){
+            setUserGlory(getUserGlory()+20);
+            System.out.println("yeah paso 3-> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(STEP_3_TEXT,
+                    Map.of("Hackear el robot aspiradora +30 de fama", "step_3_btn"));
+        }
+
+        //Que pasa si se da click en un botón...
+        if( getCallbackQueryButtonKey().equals("step_3_btn") ){
+            setUserGlory(getUserGlory()+30);
+            System.out.println("yeah paso 4-> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(STEP_4_TEXT,
+                    Map.of("Enviar al robot aspitador por comida +30 de fama", "step_4_btn",
+                            "Dar un paseo encima del robot +30 de fama", "step_4_btn",
+                            "Huir del robot aspiradora +30 de fama", "step_4_btn"));
+        }
+
+        //Que pasa si se da click en otro botón...
+        if( getCallbackQueryButtonKey().equals("step_4_btn") ){
+            setUserGlory(getUserGlory()+30);
+            System.out.println("yeah paso 5-> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(STEP_5_TEXT,
+                    Map.of("Ponerse la GoPro y encenderla +40 de fama", "step_5_btn"));
+        }
+
+        //Que pasa si se da click en otro botón...
+        if( getCallbackQueryButtonKey().equals("step_5_btn") ){
+            setUserGlory(getUserGlory()+40);
+            System.out.println("yeah paso 6-> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(STEP_6_TEXT,
+                    Map.of("Pescado +30 de fama", "step_6_btn",
+                            "Aceituna +30 de fama", "step_6_btn"));
+        }
+
+        //Que pasa si se da click en otro botón...
+        if( getCallbackQueryButtonKey().equals("step_6_btn") ){
+            setUserGlory(getUserGlory()+30);
+            System.out.println("yeah paso 7-> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(STEP_7_TEXT,
+                    Map.of("Hackear la contraseña de la computadora +50 de fama", "step_7_btn"));
+        }
+
+        //Que pasa si se da click en otro botón...
+        if( getCallbackQueryButtonKey().equals("step_7_btn") ){
+            setUserGlory(getUserGlory()+50);
+            System.out.println("yeah paso 8-> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(STEP_8_TEXT,
+                    Map.of("Convertirse en gato hacker finalmente", "step_8_btn"));
+        }
+
+        //Que pasa si se da click en otro botón...
+        if( getCallbackQueryButtonKey().equals("step_8_btn") ){
+            setUserGlory(getUserGlory());
+            System.out.println("yeah paso 9-> "+getUserGlory()+" puntos");
+            sendTextMessageAsync(FINAL_TEXT, Map.of());
         }
 
     }
